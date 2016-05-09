4735420bcce93084ccf15b555f2c2d14d67ba46d
blainekasten <blainekasten@gmail.com>
2016-05-08 23:16:47 -0500

    Fixes internal `shouldComponentUpdate` with nested wraps

--- src/wrapComponent.js
+++ src/wrapComponent.js
@@ -1,7 +1,7 @@
 /*
  * @flow
  */
-import React from 'react';
+import React, { Children } from 'react';
 import StatelessWrapper from './StatelessWrapper';
 import CofluxContainer from './CofluxContainer';
 
@@ -9,6 +9,41 @@ import CofluxContainer from './CofluxContainer';
 // memoize configs so we don't have to factory?
 //  and clear them on react-route changes
 export default function wrapComponent(Component:Function, config:Object):Function {
+  Component.dependencies = config.mapStateToProps;
+
+  /*
+   * Builds an object of Component & child/grandchild Component dependencies from
+   * the entire tree of children from this component down.
+   */
+  Component.childDependencies = function childDependencies(Component:Object, children:Array|Object) : Object {
+    const deps:Object = Component.dependencies();
+
+    let arrayChildren:Array<Object> = Children.toArray(children);
+
+    arrayChildren.forEach(buildGranchildDependencies);
+
+    function buildGranchildDependencies(child) : void {
+      const childComponent:Object = child.type.nativeComponent;
+
+      if (!childComponent) {
+        const childChildren:Array<Object> = Children.toArray(child.props.children);
+        childChildren.forEach(buildGranchildDependencies);
+        return;
+      }
+
+      const grandChildDependencies:Object = childComponent.childDependencies(
+        childComponent,
+        child.props.children
+      );
+
+      Object.keys(grandChildDependencies).forEach(
+        key => deps[key] = grandChildDependencies[key]
+      );
+    }
+
+    return deps;
+  };
+
   const wrappedFunction = function(props:?Object):React.DOM {
     return (
       <CofluxContainer
